6416b28f31ac006ec7066549d4427def16e26fad
Doritosch <als000825@naver.com>
2026-07-02 19:56:31 +0900

    Revert "User 도메인 구현"

--- src/main/java/com/talkie/chat/user/dto/PasswordUpdateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.talkie.chat.user.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
-public record PasswordUpdateRequest(
-        @NotBlank @Size(min = 8) String password
-) {
-}
--- src/main/java/com/talkie/chat/user/dto/UserUpdateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.talkie.chat.user.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record UserUpdateRequest(
-        @NotBlank String nickname,
-        String profileImageUrl
-) {
-}
