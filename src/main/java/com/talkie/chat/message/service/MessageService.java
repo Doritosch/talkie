b6f4a6e247a6fbf7f2f6afca3bba2ac40a70ca65
Doritosch <als000825@naver.com>
2026-07-13 15:55:39 +0900

    feat(message): 메시지 저장 기능 추가

--- src/main/java/com/talkie/chat/message/service/MessageService.java
+++ src/main/java/com/talkie/chat/message/service/MessageService.java
@@ -3,6 +3,10 @@ package com.talkie.chat.message.service;
 import com.talkie.chat.message.dto.MessageResponse;
 import com.talkie.chat.message.entity.Message;
 import com.talkie.chat.message.repository.MessageRepository;
+import com.talkie.chat.room.entity.Room;
+import com.talkie.chat.room.repository.RoomRepository;
+import com.talkie.chat.user.entity.User;
+import com.talkie.chat.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +19,20 @@ import java.util.List;
 public class MessageService {
 
     private final MessageRepository messageRepository;
+    private final UserRepository userRepository;
+    private final RoomRepository roomRepository;
+
+    @Transactional
+    public MessageResponse saveMessage(Long userId, Long roomId, String content) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 방입니다."));
+
+        Message message = new Message(content, user, room);
+        Message savedMessage = messageRepository.save(message);
+        return MessageResponse.from(savedMessage);
+    }
 
     public List<MessageResponse> findMessagesByRoomId(Long roomId, Long cursor, int size) {
 
